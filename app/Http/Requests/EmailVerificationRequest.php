22201e0a1b29dea54c8ca3abadd2214da05f553c
Ivan <iantonio@netlinkvoice.com>
2025-08-10 14:37:04 +0800

    verify email

---
+++ app/Http/Requests/EmailVerificationRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Auth\EmailVerificationRequest as CoreRequest;
+
+class EmailVerificationRequest extends CoreRequest
+{
+    public function user($guard = null)
+    {
+        return User::find($this->route('id'));
+    }
+}
